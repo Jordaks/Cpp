a15f6974f0b5df323885fe2f3617be8c845144c2
Jordan <javiermarkjordan@gmail.com>
2024-02-21 20:38:07 +0800

    sign in

--- getline.cpp
+++ getline.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    string yourName;
+    /*string yourName;
     string birthDay;
 
     cout << yourName << endl;
@@ -13,7 +13,40 @@ int main()
 
 
     cout << birthDay << endl;
-    getline(cin, birthDay);
+    getline(cin, birthDay);*/
+    
+
+    string mail;
+    string pass;
+    int nuM;
+    
+
+    cout << "\n   CREATE ACCOUNT\n\n";
+    
+    cout << "   Sign up\n ";
+    
+    cout << "   Log In\n\n ";
+    
+    cout << "   Type 1 to Log in\n ";
+    
+    cout << "   Type 0 to Sign up\n\n "; 
+    cin >> nuM;
+
+    if (nuM <= 0 ){
+        cout << "Sign up\n\n";
+        cout << "Email:\n ";
+        cin >> mail;
+        cout << "Password: \n";
+        cin >> pass;
+    }
+        else if(nuM >= 1){
+            cout << "Log In\n\n";
+            cout << "Email:\n ";
+            cin >> mail;
+            cout << "Password:\n ";
+            cin >> pass;
+        }
+
     
     return 0;
 }
