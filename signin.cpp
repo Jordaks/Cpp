6564b110c1b808f5b93fa72c36ec4f2a96c67f6d
Jordan <javiermarkjordan@gmail.com>
2024-02-21 21:14:01 +0800

    new

--- signin.cpp
+++ signin.cpp
@@ -1,32 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    string mail;
-    string pass;
-    int nuM;
+        string mail, pass;
+        int nuM;
     
 
-    cout << "\n   CREATE ACCOUNT\n\n";
-    
-    cout << "   Sign up\n ";
-    
-    cout << "   Log In\n\n ";
-    
-    cout << "   Type 1 to Log in\n ";
-    
-    cout << "   Type 0 to Sign up\n\n "; 
-    cin >> nuM;
+        cout << "\n   CREATE ACCOUNT\n\n";
 
-    if (nuM <= 0 ){
+        cout << "   Sign up\n ";
+
+        cout << "   Log In\n\n ";
+
+        cout << "   Type 1 to Log in\n ";
+
+        cout << "   Type 0 to Sign up\n\n "; 
+        cin >> nuM;
+
+    if (nuM <= 0 )
+    {
         cout << "Sign up\n\n";
         cout << "Email:\n ";
         cin >> mail;
         cout << "Password: \n";
         cin >> pass;
     }
-        else if(nuM >= 1){
+        else if(nuM >= 1)
+        {
             cout << "Log In\n\n";
             cout << "Email:\n ";
             cin >> mail;
