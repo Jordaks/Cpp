5570bf6a3f019578ffc15d6bb9730698153a4441
Jordan <javiermarkjordan@gmail.com>
2024-02-22 22:29:44 +0800

    co

--- constant.cpp
+++ constant.cpp
@@ -12,3 +12,18 @@ int main()
     cout << x;
     
 }
+
+/*#include <iostream>
+using namespace std;
+
+const int myConstant = 10; // Global constant
+
+int main() {
+    const int myConstant = 5; // Local constant in main function
+
+    cout << "Local constant in main function: " << myConstant << endl; // Outputs the local constant
+    cout << "Global constant: " << ::myConstant << endl; // Outputs the global constant using the scope resolution operator (::)
+
+    return 0;
+}
+*/
