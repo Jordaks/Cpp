2ec6e545cb8bedc3fb15611469d7035e19997ee6
Jordan <javiermarkjordan@gmail.com>
2024-02-29 22:13:47 +0800

    not done

---
+++ calcu.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+     
+        int num1;
+        int num2;
+        //int sign;
+        string sign = "+";
+        string sign = "-";
+        string sign = "/";
+        string sign = "*";
+        string sign = "%";
+            
+
+            /*cout << " Enter a number: \n";
+            cin >> num1;
+            cout << " Enter a number: \n";
+            cin >> num2;*/
+            
+            cout << "Choose operator: 1 = +, 2 = -,3 = /, 4 = *,5 = % \n";
+            cout << "Enter a number: \n";
+            cin >> sign;
+          
+    
+
+          //cout << " The answer is " << answer << "\n";
+            
+        /*switch (operator)
+        {
+        
+            case 1:
+                cout << "+";
+                break;
+
+            case 2:
+                cout << "-";
+                break;
+
+            case 3:
+                cout << "*";
+                break;
+
+            case 4:
+                cout << "/";
+                break;
+
+            case 5:
+                cout << "%";
+                break;
+
+            default:
+                cout << "Invalid Operator!!!";
+                break;
+
+      }*/
+            
+
+            
+
+    return 0;
+}
