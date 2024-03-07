c45cc69608d0b1adfbe18e0465beb2826fb39a7c
Jordan <javiermarkjordan@gmail.com>
2024-03-07 19:44:48 +0800

    done

---
+++ triangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    for (int i = 0; i < 1; i++){
+        cout << "  " << '*' << "\n"<< " ";
+        for (int  j = 0; j < 3; j++){
+            cout  << '*';     
+        }
+        cout << "\n";
+        for (int k = 0; k < 5; k++){
+            cout << '*';
+        }
+    }
+    return 0;
+}
