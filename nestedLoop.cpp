af00861e75edf7d08408895ea23d12983e13451f
Jordan <javiermarkjordan@gmail.com>
2024-03-12 18:58:26 +0800

    d

---
+++ nestedLoop.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+        //  nested loop
+
+       /*for(int i = 0; i < 3; i++){
+            cout << "Outer" << i << "\n";
+            for(int j = 1; j < 4; j++){
+                cout << " Inner" << j << "\n";
+            }
+        } 
+
+        //while loop
+
+        int i = 1;
+
+        while( i < 5){
+            cout << i << "\n";
+            i++;
+        }*/
+        
+        //do/while loop
+        
+        int i = 2;
+
+        do {
+            cout << i << "\n";
+            i++;
+        }while(i <= 6);
+        
+    return 0;
+}
